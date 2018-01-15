443fd9224216ed6bdf72fc2394e9802f361ecc42
cngmsy <chj294671171@126.com>
2018-01-15 20:49:34 +0800

    Cr高清图加载

---
+++ JiaZaiJuTu/app/src/main/java/com/example/admin/gaoqingdatu/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.admin.gaoqingdatu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+
+public class MainActivity extends AppCompatActivity {
+    private int screenX, screenY;
+    private int width, height;
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
