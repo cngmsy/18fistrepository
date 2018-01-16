9e2df71109ba001108319b30ea4e529ea5ca001b
WilliamEyre <1764750372@qq.com>
2018-01-16 15:54:29 +0800

    图片压缩与优化（可相册获取图片、拍照获取图片）

---
+++ OptimizeImage/app/src/main/java/com/example/laptop/optimizeimage/MainActivity.java
@@ -0,0 +1,120 @@
+package com.example.laptop.optimizeimage;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.example.laptop.optimizeimage.util.MPermissionsUtils;
+import com.example.laptop.optimizeimage.util.camera.CameraCore;
+import com.example.laptop.optimizeimage.util.camera.CameraProxy;
+
+import java.io.File;
+
+import me.xiaosai.imagecompress.ImageCompress;
+
+/*
+ * 图片压缩与优化
+ */
+
+public class MainActivity extends Activity implements CameraCore.CameraResult {
+    private Button choose_image,camera_image;
+    private CameraProxy cameraProxy;
+    private ImageView choose_bit;
+    /** SD卡根目录 */
+    private final String externalStorageDirectory = Environment.getExternalStorageDirectory().getPath()+"/picture/";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //压缩后保存临时文件目录
+        File tempFile = new File(externalStorageDirectory);
+        if(!tempFile.exists()){
+            tempFile.mkdirs();
+        }
+        cameraProxy = new CameraProxy(this, MainActivity.this);
+        choose_image = (Button) findViewById(R.id.choose_image);
+        choose_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cameraProxy.getPhoto2Album();
+            }
+        });
+        choose_bit = (ImageView) findViewById(R.id.choose_bit);
+        camera_image = (Button) findViewById(R.id.camera_image);
+        camera_image.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                String cameraPath = externalStorageDirectory+System.currentTimeMillis()/1000+".jpg";
+                cameraProxy.getPhoto2Camera(cameraPath);
+            }
+        });
+    }
+
+    //拍照选图片成功回调
+    @Override
+    public void onCameraSuccess(final String filePath) {
+        File file = new File(filePath);
+        if (file.exists()) {
+            ImageCompress.with(this)
+                    .load(filePath)
+                    .setTargetDir(externalStorageDirectory)
+                    .ignoreBy(150)
+                    .setOnCompressListener(new ImageCompress.OnCompressListener() {
+                        @Override
+                        public void onStart() {
+                            Log.e("compress", "onStart");
+                        }
+
+                        @Override
+                        public void onSuccess(String filePath) {
+                            Log.e("compress", "onSuccess=" + filePath);
+                            choose_bit.setImageBitmap(BitmapFactory.decodeFile(filePath));
+                        }
+
+                        @Override
+                        public void onError(Throwable e) {
+                            e.printStackTrace();
+                            Log.e("compress", "onError");
+                        }
+                    }).launch();
+            }
+    }
+
+    //拍照选图片失败回调
+    @Override
+    public void onCameraFail(String message) {
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        MPermissionsUtils.getInstance().onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        cameraProxy.onResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        cameraProxy.onRestoreInstanceState(savedInstanceState);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        cameraProxy.onSaveInstanceState(outState);
+    }
+}
